b288e1144b003572da47b62ce8bfde5d71afc527
Tore Sæterdal <tore.1992@gmail.com>
2020-11-22 20:26:06 +0100

    add filtering to display projects by selection

--- github-query.js
+++ github-query.js
@@ -1,7 +1,9 @@
 jQuery.githubUser = function(username, callback) {
     jQuery.getJSON('https://api.github.com/users/'+username+'/repos?callback=?',callback)
  }
-  
+
+var displayRepos = ["challenges", "ansible-gitops", "Sanntid","rh_sat_prometheus_targets", "kotlin-snake","puppet-module-unbound"];
+
  jQuery.fn.loadRepositories = function(username) {
      this.html("<span>Querying GitHub for " + username +"'s repositories...</span>");
       
@@ -13,7 +15,7 @@ jQuery.githubUser = function(username, callback) {
          var list = $('<dl/>');
          target.empty().append(list);
          $(repos).each(function() {
-             if (this.name != (username.toLowerCase()+'.github.com')) {
+             if (this.name != (username.toLowerCase()+'.github.com') && displayRepos.includes(this.name) ) {
                  list.append('<dt><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a> <em>'+(this.language?('('+this.language+')'):'')+'</em></dt>');
                  list.append('<dd>' + this.description +'</dd>');
              }
